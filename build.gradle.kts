fa6a5d99f8482b4fc478de86fdb7f07246e608e8
leogracas34-gif <leogracas34@gmail.com>
2026-01-28 23:45:23 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     // Atualizado para 8.2.2 para suporte total ao Java 17 e Firebase 34
     id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    // Atualizado para 1.9.24 para resolver o erro de incompatibilidade de Metadata do Kotlin
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
