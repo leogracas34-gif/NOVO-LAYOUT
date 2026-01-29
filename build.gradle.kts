db57b851022d2a40c5341324bee4a173c07c6376
leogracas34-gif <leogracas34@gmail.com>
2026-01-28 23:28:37 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    // Atualizado para 8.2.2 para suporte total ao Java 17 e Firebase 34
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
